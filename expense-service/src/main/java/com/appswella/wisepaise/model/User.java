cd09193dcad8766c8f4ad07e35ebc6c7ad32bc9f
mnshaikna <mnshaikna@gmail.com>
2025-12-08 17:18:27 +0400

    user model updated

--- expense-service/src/main/java/com/appswella/wisepaise/model/User.java
+++ expense-service/src/main/java/com/appswella/wisepaise/model/User.java
@@ -1,5 +1,6 @@
 package com.appswella.wisepaise.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,5 +19,6 @@ public class User {
     private String userEmail;
     private String userImageUrl;
     private String userCreatedOn;
+    @JsonProperty("isRegistered")
     private boolean isRegistered;
 }
