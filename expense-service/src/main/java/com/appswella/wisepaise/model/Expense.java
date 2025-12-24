367bdd6fee4f58413a0fe436ec8098216dd52215
mnshaikna <mnshaikna@gmail.com>
2025-12-24 14:12:31 +0400

    Settlement Transaction color code

--- expense-service/src/main/java/com/appswella/wisepaise/model/Expense.java
+++ expense-service/src/main/java/com/appswella/wisepaise/model/Expense.java
@@ -1,5 +1,6 @@
 package com.appswella.wisepaise.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,5 +30,7 @@ public class Expense {
     private List<String> expensePaidTo;
     private String expenseReceiptURL;
     private String expenseUserId;
+
+    @JsonProperty("isSettlement")
     private boolean isSettlement;
 }
