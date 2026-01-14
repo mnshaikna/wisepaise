126996a40fdbf395d3e154fd8194f2daf0204562
mnshaikna <mnshaikna@gmail.com>
2026-01-14 17:54:39 +0400

    JWT Implementation - In Progress

--- api-gateway/src/main/java/com/appswella/wisepaise/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/appswella/wisepaise/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.appswella.wisepaise.config;
 
-import com.appswella.wisepaise.security.JwtWebFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
